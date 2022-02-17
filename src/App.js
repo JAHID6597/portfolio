08e07a519dbe3d0920cc83b927b49f72eae3851e
unknown <1952jahid1971@gmail.com>
2022-02-18 01:08:54 +0600

    Update app.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Main from './container/Main';
-import './app.css';
+import './App.css';
 
 const App = () => {
   return (
